d1ed67b590913a5c97a8900a36e16160b8d67950
Vincent Tavernier <v.tavernier@pm.me>
2023-01-02 00:30:19 +0100

    build(update-pinned-actions): add support for action dependencies

--- ci/update-pinned-actions.sh
+++ ci/update-pinned-actions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eu
 
 source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
 
@@ -33,16 +33,16 @@ for ACTION in actions/*; do
 
   if [[ $CHECK_MODE == 1 ]]; then
     # Check that there are no references to the outdated action
-    readarray -t OUTDATED_FILES < <(grep -P "vtavernier/github-workflows/$ACTION@(?!$HEAD_REF)" -r -n .github/workflows)
+    readarray -t OUTDATED_FILES < <(grep -P "vtavernier/github-workflows/$ACTION@(?!$HEAD_REF)" -r -n .github/workflows actions/*/action.yml)
     for FILE in "${OUTDATED_FILES[@]}"; do
-      echo "::error file=$(awk -F: '{print $1}' <<< "$FILE"),line=$(awk -F: '{print $2}' <<< "$FILE")::$ACTION pinned version is outdated. Latest version: $HEAD_REF. Current version in workflow: $(awk -F@ '{print $NF}' <<< "$FILE")"
+      echo "::error file=$(awk -F: '{print $1}' <<< "$FILE"),line=$(awk -F: '{print $2}' <<< "$FILE")::$ACTION pinned version is outdated. Latest version: $HEAD_REF. Current version in dependent: $(awk -F@ '{print $NF}' <<< "$FILE")"
 
       ERRORS=$((ERRORS + 1))
     done
   else
     # Replace the reference to the action with the pinned commit
-    dry_run sed -i "s#\(vtavernier/github-workflows/$ACTION@\)[0-9a-f]\{40\}#\1$HEAD_REF#g" \
-      .github/workflows/*.yml
+    dry_run sed -i "s#\(vtavernier/github-workflows/$ACTION@\)[^ ]*#\1$HEAD_REF#g" \
+      .github/workflows/*.yml actions/*/action.yml
   fi
 done
 
